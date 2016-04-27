aa9a1adda45ae91c1dbdaa3dc79c70b4877dab27
James Stanley Targett <jme19t@gmail.com>
2016-04-27 17:14:13 +0100

    separate out dumb routing logic

--- ocado_sandpit/src/main/Main.java
+++ ocado_sandpit/src/main/Main.java
@@ -42,26 +42,15 @@ public class Main {
 				if (r.currentLocation.equals(r.destination)) {
 					r.goal = true;
 					r.destination = new MyPoint(rand.nextInt(grid.getxGridSize()), rand.nextInt(grid.getyGridSize()));
-
-			}
-
-			for (Robot r : grid.getGrid()) {
-				 
-				if (r.currentLocation.x != r.destination.x) {
-					if (r.currentLocation.x > r.destination.x)
-						r.nextLocation = r.currentLocation.left();
-					else
-						r.nextLocation = r.currentLocation.right();
-					r.nextLocation.y = r.currentLocation.y;
-				} else {
-					if (r.currentLocation.y > r.destination.y)
-						r.nextLocation = r.currentLocation.up();
-					else
-						r.nextLocation = r.currentLocation.down();
-					r.nextLocation.x = r.currentLocation.x;
 				}
+				
+
 			}
 			
+			
+
+			dumbLRouting(grid);
+
 				
 
 			for (Robot r : grid.getGrid()) {
@@ -69,12 +58,35 @@ public class Main {
 				for (Robot o : grid.getGrid()) {
 					if (r == o) continue;
 					if (o.currentLocation.equals(r.nextLocation) || o.nextLocation.equals(r.nextLocation)) {
-						r.blocked = true;
-						break;
+						if (r.nextLocation.x==r.currentLocation.x) {
+							r.blocked = true;
+							break;
+						}
 					}
 				}
 			}
+			
 			frame.repaint();
 		}
+	}
+
+
+	private static void dumbLRouting(Grid grid) {
+		for (Robot r : grid.getGrid()) {
+			if (r.currentLocation.x != r.destination.x) {
+				if (r.currentLocation.x > r.destination.x)
+					r.nextLocation = r.currentLocation.left();
+				else
+					r.nextLocation = r.currentLocation.right();
+				r.nextLocation.y = r.currentLocation.y;
+			} else {
+				if (r.currentLocation.y > r.destination.y)
+					r.nextLocation = r.currentLocation.up();
+				else
+					r.nextLocation = r.currentLocation.down();
+				r.nextLocation.x = r.currentLocation.x;
+			}
+		}
+		
 	}
 }
