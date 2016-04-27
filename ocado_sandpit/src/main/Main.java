d94f8fb22b9e0c373bba33352e8b4ddc9f14dd25
pgaref <pgaref@macbook-air.lan>
2016-04-27 21:27:09 +0100

    Puting to the main JFrame

--- ocado_sandpit/src/main/Main.java
+++ ocado_sandpit/src/main/Main.java
@@ -1,29 +1,31 @@
 package main;
 
-import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.Random;
 
 import javax.swing.JFrame;
 
+import org.jfree.data.time.Millisecond;
+
 import Utils.MyPoint;
 import Utils.Statistics;
 import navigation.Grid;
 import navigation.Robot;
 import view.GridViewPanel;
-import view.LinePlotPanel;
+import view.StatsPanel;
+import view.TimeSeriesChart;
 
 public class Main {
 
 	static int total_robots = 20;
 	static int PANEL_COUNT = 2;
 	static Statistics stats = new Statistics();
-	
+
 	public static void main(String[] args) throws InterruptedException {
 		Grid grid = new Grid(total_robots);
 		grid.initGrid();
-		
+
 		JFrame frame = new JFrame();
 		frame.setTitle("Ocado Challenge - Sandpit 2016");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,67 +33,93 @@ public class Main {
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 0;
 		gbc.gridy = 0;
+		gbc.gridwidth = 2;
 		gbc.weightx = 1.0;
 		gbc.weighty = 1.0;
-		gbc.fill = GridBagConstraints.BOTH;		
+		gbc.fill = GridBagConstraints.BOTH;
 		frame.add(new GridViewPanel(grid), gbc);
 		gbc.gridy = 1;
+		gbc.gridwidth = 1;
+		gbc.weightx = 0;
 		gbc.weighty = 0;
 		gbc.fill = GridBagConstraints.BOTH;
-		frame.add(new LinePlotPanel(stats), gbc);
+		
+		StatsPanel spanel = new StatsPanel(stats);
+		frame.add(spanel, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		TimeSeriesChart throughputChart = new TimeSeriesChart();
+		frame.add(throughputChart.initTimeSeriesChart("Average Throughput") , gbc);
 		frame.setSize(820, 650);
 		frame.setVisible(true);
-		
+
+		int currentCollisions = 0;
+		int currentCompletions = 0;
+		long timer = System.currentTimeMillis();
+
 		Random rand = new Random();
 		while (true) {
 			Thread.sleep(100);
 
 			for (Robot r : grid.getGrid()) {
-				
-				r.goal = false;
-				
-				if (r.blocked){
-					stats.addCollision();
-					continue;
-				}
-				else{
-					r.currentLocation = r.nextLocation;
 
-				if (r.currentLocation.equals(r.destination)) {
-					r.goal = true;
-				}
-				if (r.currentLocation.equals(r.destination)){
-					stats.addCompletion();
-					r.destination = new MyPoint(rand.nextInt(grid.getxGridSize()), rand.nextInt(grid.getyGridSize()));
+				r.goal = false;
+
+				if (r.blocked) {
+					currentCollisions += 1;
+					continue;
+				} else {
+					r.currentLocation = r.nextLocation;
+					if (r.currentLocation.equals(r.destination)) {
+						currentCompletions += 1;
+						r.goal = true;
+						r.destination = new MyPoint(rand.nextInt(grid.getxGridSize()),
+								rand.nextInt(grid.getyGridSize()));
+					}
 				}
 				
+				/*
+				 * Update stats every seconds
+				 */
+				if(System.currentTimeMillis() - timer > 1000){
+					
+					stats.getCollisions().add(currentCollisions);
+					stats.getCompletions().add(currentCompletions);
+					
+					final Millisecond now = new Millisecond();
+					throughputChart.getSeries().add(now, stats.getAverageThroughput());
+					System.out.println("Avg Thr: "+ stats.getAverageThroughput() + " Completions: "+ stats.getCompletionSum() +" Collisions: "+stats.getCollistionSum());
+					
+					currentCollisions = 0;
+					currentCompletions = 0;
+					timer=System.currentTimeMillis();
 				}
+				
 
 			}
-			
-			
 
 			dumbLRouting(grid);
-
-				
 
 			for (Robot r : grid.getGrid()) {
 				r.blocked = false;
 				for (Robot o : grid.getGrid()) {
-					if (r == o) continue;
+					if (r == o)
+						continue;
 					if (o.currentLocation.equals(r.nextLocation) || o.nextLocation.equals(r.nextLocation)) {
-						if (r.nextLocation.x==r.currentLocation.x) {
+						if (r.nextLocation.x == r.currentLocation.x) {
 							r.blocked = true;
 							break;
 						}
 					}
 				}
 			}
-			
+
 			frame.repaint();
 		}
 	}
-
 
 	private static void dumbLRouting(Grid grid) {
 		for (Robot r : grid.getGrid()) {
@@ -109,6 +137,7 @@ public class Main {
 				r.nextLocation.x = r.currentLocation.x;
 			}
 		}
-		
+
 	}
+
 }
