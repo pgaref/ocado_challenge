3259655bf0bc24d3224bffe7f122c6a9910662f1
Salvatore Dipietro <s.dipietro14@imperial.ac.uk>
2016-04-28 10:50:00 +0100

    few changes

--- ocado_sandpit/src/main/StatisticsMain.java
+++ ocado_sandpit/src/main/StatisticsMain.java
@@ -4,12 +4,12 @@ public class StatisticsMain {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		Main.main(args, 10, 1, 10000);
+//		Main.main(args, 10, 1, 10000);
 
-//		for (int i = 25; i <= 1000; i += 25) {
-//			Main.main(args, i, 1, 10000);
-//
-//		}
+		for (int i = 25; i <= 1000; i += 25) {
+			Main.main(args, i, 1, 1000000);
+
+		}
 	}
 
 }
